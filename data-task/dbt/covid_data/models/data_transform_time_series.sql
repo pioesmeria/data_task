7b6dcb507fd1bb539b35de72add9a9f0d683ebc1
pioesmeria <46041054+pioesmeria@users.noreply.github.com>
2024-09-10 23:37:21 +0800

    Delete data-task directory

--- data-task/dbt/covid_data/models/data_transform_time_series.sql
+++
@@ -1,9 +0,0 @@
-{{
-  config(
-    materialized='view', unlogged=True
-  )
-}}
-
-SELECT
-    *
-FROM {{ source('mage_covid_data','time_series_covid_data_time_series_load_data') }}
--- data-task/dbt/covid_data/models/data_transform_time_series_us.sql
+++
@@ -1,9 +0,0 @@
-{{
-  config(
-    materialized='view', unlogged=True
-  )
-}}
-
-SELECT
-    *
-FROM {{ source('mage_covid_data','us_time_series_covid_data_us_time_series_load_data') }}
